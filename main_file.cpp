0070ff9f35f49b45312d299b01ffa648fa62c63a
me05458 <193655209+me05458@users.noreply.github.com>
2026-03-17 17:27:36 -0600

    Create main_file.cpp

---
+++ main_file.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "w_String.h"
+#include "random_file.h"
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+
+int main() {
+  printf("Hello World! %d%d%d\n",1,2,3);
+  cout<<"Hello World! "<<1<<2<<3<<std::endl;
+  return 0;
+}
